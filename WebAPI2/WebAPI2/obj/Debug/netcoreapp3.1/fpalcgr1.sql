4b93edf10a103b5ba377de593c67aad6f012ef1e
Hirushka Nirmani <31449703+hirushka@users.noreply.github.com>
2021-09-17 09:53:48 +0530

    Add files via upload
    Initial

---
+++ WebAPI2/WebAPI2/obj/Debug/netcoreapp3.1/fpalcgr1.sql
@@ -0,0 +1,39 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210902133534_InitialCreate')
+BEGIN
+    CREATE TABLE [Students] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(100) NULL,
+        [Email] nvarchar(100) NULL,
+        [Age] int NOT NULL,
+        CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+         );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210902133534_InitialCreate')
+BEGIN
+    CREATE INDEX [IX_Students_StudentId] ON [Students] ([StudentId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210902133534_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210902133534_InitialCreate', N'3.1.0');
+END;
+
+GO
+
